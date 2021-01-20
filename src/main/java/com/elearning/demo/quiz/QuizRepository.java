bd32ffd780f6b0f6e3d483393909481d1220d0c4
Nguyễn Bùi Quang Minh <67447333+minhnguyen8219@users.noreply.github.com>
2021-01-21 00:33:09 +0700

    update query countQuizByCategoryId in QuizRepository

--- src/main/java/com/elearning/demo/quiz/QuizRepository.java
+++ src/main/java/com/elearning/demo/quiz/QuizRepository.java
@@ -1,8 +1,15 @@
 package com.elearning.demo.quiz;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    @Query("select s.category_id, count(category_id) from quiz s where s.category_id = :id")
+    Long countQuizByCategoryId(@Param("userId") Long id);
+
 }
